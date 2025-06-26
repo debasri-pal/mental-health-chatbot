8c1cd92cffd8ed672369d9bf6259d53595717341
Debasri Pal <debasripal238@gmail.com>
2025-06-26 11:07:26 +0530

    Fix: Re-added frontend folder for full visibility on GitHub

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// UI rendered by Debasri Pal 😊
+
+import React from "react";
+import Chatbot from "./components/Chatbot";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <Chatbot />
+    </div>
+  );
+}
+export default App;
+
